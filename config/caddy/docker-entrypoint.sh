1eae4913c9a78087224cf4344fcd8fa88e90e6f5
Enrique Pascalin <erparom@gmail.com>
2025-06-17 16:05:57 -0600

    Fixes structure

--- config/caddy/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -e
-# wait until Postgres accepts connections
-until php /app/bin/console doctrine:query:sql "SELECT 1" >/dev/null 2>&1; do
-  echo "⏳ waiting for database…" && sleep 2
-done
-php /app/bin/console doctrine:migrations:migrate --no-interaction
-exec "$@"
